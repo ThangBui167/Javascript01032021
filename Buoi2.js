3189d07d95c694f24c00b77ec0a656c85b41ff39
Thangbui167 <thangbui232@gmail.com>
2021-03-05 15:38:48 +0200

    higher order function

--- Buoi2.js
+++ Buoi2.js
@@ -10,3 +10,19 @@ const arrNums = [1, 2, 3, 4 ,5];
 //Xoa o giua: arrNums.splice(1,1): numb=2 will be delete
 //Them o giua: arrNums.splice(1,0,6, 7, 8), 11 is position, 0 is no delete, 6,7,8 are added values
 //arrNums.splice(-2,1)
+
+// 14: Higher order function: phuogn thuc se nhan gia tri la phuong
+
+function tinhTong(arr, fn) {
+    let result = 0;
+    for (const value of arr) {
+        result += value;
+    }
+    fn(result);
+}
+tinhTong([1, 2,3,4],console.log);// cach 1
+
+tinhTong([1, 2,3,4], function(result){ // cach 2, tu dinh nghia function
+    console.log(result)
+}) ;
+
