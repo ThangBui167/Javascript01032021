9eac86b2a464986a046e6be9b2614a80ed01373b
Thangbui167 <thangbui232@gmail.com>
2021-03-05 15:50:57 +0200

    function map

--- Buoi2.js
+++ Buoi2.js
@@ -1,6 +1,6 @@
 // 13: Array method
 
-const arrNums = [1, 2, 3, 4 ,5];
+//const arrNums = [1, 2, 3, 4 ,5];
 
 //Them du lieu o cuoi mang
 ////arrNums.push(6)
@@ -26,3 +26,10 @@ tinhTong([1, 2,3,4], function(result){ // cach 2, tu dinh nghia function
     console.log(result)
 }) ;
 
+//15 : Ham map
+// 1 - Thay doi gia tri cua phan tu, khong thay doi kich thuoc mang
+// 2 - Khong gan su thay doi vao mang cu, gan su thay doi vao mang moi
+const arrNums = [1,2,3,4,5,6,7,8,9]
+const newArrNums = arrNums.map(function (number, index){
+    console.log(number)
+})
