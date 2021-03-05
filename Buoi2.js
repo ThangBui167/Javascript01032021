2d32864b34d47b17ea3956db7426ff392d303c20
Thangbui167 <thangbui232@gmail.com>
2021-03-05 15:22:52 +0200

    array

---
+++ Buoi2.js
@@ -0,0 +1,12 @@
+// 13: Array method
+
+const arrNums = [1, 2, 3, 4 ,5];
+
+//Them du lieu o cuoi mang
+////arrNums.push(6)
+//Xoa o cuoi : arrNums.pop()
+//Them o dau: arrNums.unshift(0)
+// Xoa o dau: arrNums.shift()
+//Xoa o giua: arrNums.splice(1,1): numb=2 will be delete
+//Them o giua: arrNums.splice(1,0,6, 7, 8), 11 is position, 0 is no delete, 6,7,8 are added values
+//arrNums.splice(-2,1)
